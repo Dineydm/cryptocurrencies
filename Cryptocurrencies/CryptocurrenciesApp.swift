9f1b73d0f853db46d2428702e13572078b0aadaf
Diney <dineydm@gmail.com>
2021-11-17 09:47:54 -0300

    Initial Commit

---
+++ Cryptocurrencies/CryptocurrenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrenciesApp.swift
+//  Cryptocurrencies
+//
+//  Created by Valdiney Menezes on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
